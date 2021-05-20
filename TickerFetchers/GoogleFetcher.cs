ecd42cdfbf5eaa9d2ab2fb560edcb27a38b679b5
Dominic Guo <64115474+domguo@users.noreply.github.com>
2021-05-20 05:14:55 -0700

    Finished GetMarketData for GoogleFetcher

--- TickerFetchers/GoogleFetcher.cs
+++ TickerFetchers/GoogleFetcher.cs
@@ -8,7 +8,8 @@ using HtmlAgilityPack;
 
 
 namespace PortfolioTracker.TickerFetchers
-{    class GoogleFetcher : ITickerFetcher
+{
+    class GoogleFetcher : ITickerFetcher
     {
         private const string URL = "https://www.google.com/finance/quote/";
         //private readonly string[] exchanges = { "NASDAQ", "NYSE" };
@@ -89,28 +90,35 @@ namespace PortfolioTracker.TickerFetchers
                 range[2].Remove(0, 1);
 
                 string[] market = marketcap.Split(' ');
-                char mult = market[0][market[0].Length];
-                string marketprice = market[0].Remove(market[0].Length - 1);
+                char mult = market[0][market[0].Length - 1];
+                marketcap = market[0].Remove(market[0].Length - 1);
 
-                double mp = Convert.ToDouble(marketprice);
+                double mc = Convert.ToDouble(marketcap);
 
                 if (mult == 'M')
-                    mp = mp * 1000000;
+                    mc = mc * 1000000;
 
                 if (mult == 'B')
-                    mp = mp * 1000000000;
+                    mc = mc * 1000000000;
 
+                mult = volume[volume.Length - 1];
+                volume.Remove(volume.Length - 1);
+                double vol = Convert.ToDouble(volume);
 
+                if (mult == 'M')
+                    vol = vol * 1000000;
 
+                if (mult == 'B')
+                    vol = vol * 1000000000;
 
-                MarketData md = new MarketData(name, Convert.ToDouble(price), );
+                MarketData data = new MarketData(name, Convert.ToDouble(price), mc, Convert.ToDouble(range[2]), Convert.ToDouble(range[0]), vol);
 
-                return md;
+                return data;
 
             }
             catch (Exception) { }
 
-            MarketData md = new MarketData("asdfasdf", 0, 0, 0, 0, 0);
+            MarketData md = new MarketData("", 0, 0, 0, 0, 0);
             return md;
         }
     }
